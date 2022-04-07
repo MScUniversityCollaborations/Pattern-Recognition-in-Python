1adb02a7e47836dc5384a0f097b809bc87beb114
zartilas <zartilasvasilios@protonmail.com>
2022-04-08 00:49:41 +0300

    Second perceptron implementation

--- linearNN.py
+++ linearNN.py
@@ -1,21 +1,39 @@
 import numpy as np
 
-inputs = np.array([20, 23])
-weights = np.array([0.9, 0.1])
+# Inputs / Outputs / Weights
+inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+outputs = np.array([0, 0, 0, 1])
+weights = np.array([0.0, 0.0])
+learning_rate = 0.1
 
 
-def sum_function(inputs, weights):
-    return inputs.dot(weights)
-
-
-s = sum_function(inputs, weights)
-
-
+# Step Function
 def step_function(sum):
     if sum >= 1:
         return 1
     return 0
 
 
+# Calculate Output
+def calculate_output(instance):
+    s = instance.dot(weights)
+    return step_function(s)
+
+
+def train():
+    total_error = 1
+    while total_error != 0:
+        total_error = 0
+        for i in range(len(outputs)):
+            prediction = calculate_output(inputs[i])
+            error = abs(outputs[i] - prediction)
+            total_error += error
+            if error > 0 :
+                for j in range(len(weights)):
+                    weights[j] = weights[j] + (learning_rate * inputs[i][j] * error)
+                    print('Weight update: ' + str(weights[j]))
+        print('Total error: ' + str(total_error))
+
+
 def start():
-    print(step_function(s))
+    print(train())
